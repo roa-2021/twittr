236358d4ddfdbb02c0901b1b7648b5e059492a17
Ross Jourdain <>
2020-10-27 11:15:35 +1300

    Reshuffle the layout

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 module.exports = {
   entry: ["./client/index.js", "./client/styles/index.scss"],
   output: {
-    path: path.join(__dirname, "public"),
+    path: path.join(__dirname, "..", "server", "public"),
     filename: "bundle.js",
   },
   mode: "development",
--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require("express")
+const path = require("path")
+
 
 const server = express()
 
 server.use(express.json())
-server.use(express.static("public"))
+server.use(express.static(path.join("server", "public")))
 
 module.exports = server
