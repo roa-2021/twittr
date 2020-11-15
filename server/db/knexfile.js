a001bfd6b3db87404be7f786db50747d6de5eda5
Ross Jourdain <>
2020-11-16 12:39:27 +1300

    Fix knexfile

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -1,10 +1,10 @@
-// Update with your config settings.
+const path = require('path')
 
 module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: path.join(__dirname, 'dev.sqlite3')
     },
     useNullAsDefault: true
   },
