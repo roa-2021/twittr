f7671552a8f2a9acd55db1d32d8b1c71461a45fc
Patrick John Sullivan <17734388+patrick-j-sulley@users.noreply.github.com>
2021-09-23 11:49:15 +1200

    Set up base server side auth routes

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const path = require('path')
 
+const authRoutes = require('./routes/auth')
+
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
+
+server.use('/api/v1', authRoutes)
 
 module.exports = server
