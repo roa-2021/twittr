19a93fa2da4e294fa0d0d9f51bd7030ad3e3657a
Gao Kang <gaokang@TheOnePlus6T.localdomain>
2021-09-23 09:56:41 +1200

    foreignkey

--- server/db/migrations/20210922113309_comments.js
+++ server/db/migrations/20210922113309_comments.js
@@ -3,7 +3,8 @@
 exports.up = function(knex) {
   return knex.schema.createTable('comments',function(table){
     table.increments('id').primary()
-    table.integer('publisher_id')
+    table.integer('publisher')
+    table.integer('tweet_id')
     table.datetime('publish_date')
     table.string('content')
   })
