4d3b8417e1fc73c3a6e9add00d4f25908b7f6f0e
Patrick John Sullivan <17734388+patrick-j-sulley@users.noreply.github.com>
2021-09-23 11:19:40 +1200

    login/logout implementation WIP

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import { Auth0Provider } from "@auth0/auth0-react";
+import { useAuth0 } from "@auth0/auth0-react";
 //
 import Landing from './Landing'
+import LoginButton from './LoginButton';
+import LogoutButton from './LogoutButton';
 
-function App () {
+function App() {
+
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  console.log('Authentication status: ' + isAuthenticated)
+
   return (
     <>
-      <Landing />
+      <Auth0Provider
+        domain="team-twitter.au.auth0.com"
+        clientId="OS7B5yBiy5uWbmH7EOqwMwzoF8FdFWxD"
+        redirectUri={window.location.origin}
+        audience="https://team-twitter.au.auth0.com/api/v2/"
+        scope="read:current_user update:current_user_metadata"
+      >
+        {isAuthenticated ? <LogoutButton/> : <LoginButton/>}
+        <Landing />
+      </Auth0Provider>
     </>
   )
 }
