7bdc77f6be937194aac31690fdc7b36a2f7c1cb1
Jordan Cooper <jordancooper1195@gmail.com>
2021-09-23 16:34:26 +1200

    making a start on postsReducer + actions/thunks

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
 
 import userReducer from './userReducer'
+import postsReducer from './postsReducer'
 
 export default combineReducers({
-  userReducer
+  userReducer,
+  postsReducer
 })
