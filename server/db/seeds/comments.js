55f8bd196400271d83da90f7012296edc19af082
Jordan Cooper <jordancooper1195@gmail.com>
2021-09-24 20:23:26 +1200

    changes to and inclusion of routes for comments

--- server/db/seeds/comments.js
+++ server/db/seeds/comments.js
@@ -5,14 +5,62 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('comments').insert([
-        {id: 1, publisher:3, tweet_id: 2, publish_date: '06-06-2021 00:00:00', content: 'good tweet!'},
-        {id: 2, publisher:2, tweet_id: 1, publish_date: '06-06-2021 00:00:00', content: 'good tweet!'},
-        {id: 3, publisher:4, tweet_id: 3, publish_date: '06-06-2021 00:00:00', content: 'good tweet!'},
-        {id: 4, publisher:3, tweet_id: 4, publish_date: '06-06-2021 00:00:00', content: 'good tweet!'},
-        {id: 5, publisher:5, tweet_id: 2, publish_date: '06-06-2021 00:00:00', content: 'good tweet!'},
-        {id: 6, publisher:8, tweet_id: 3, publish_date: '06-06-2021 00:00:00', content: 'good tweet!'},
-        {id: 7, publisher:6, tweet_id: 4, publish_date: '06-06-2021 00:00:00', content: 'good tweet!'},
-        {id: 8, publisher:1, tweet_id: 1, publish_date: '06-06-2021 00:00:00', content: 'good tweet!'},
+        {
+          id: 1, 
+          publisher:3, 
+          tweet_id: 2, 
+          publish_date: '06-06-2021 00:00:00', 
+          content: 'good tweet!'
+        },
+        {
+          id: 2, 
+          publisher:2, 
+          tweet_id: 1, 
+          publish_date: '06-06-2021 00:00:00', 
+          content: 'good tweet!'
+        },
+        {
+          id: 3, 
+          publisher:4, 
+          tweet_id: 3, 
+          publish_date: '06-06-2021 00:00:00', 
+          content: 'good tweet!'
+        },
+        {
+          id: 4, 
+          publisher:3, 
+          tweet_id: 4, 
+          publish_date: '06-06-2021 00:00:00', 
+          content: 'good tweet!'
+        },
+        {
+          id: 5, 
+          publisher:5, 
+          tweet_id: 2, 
+          publish_date: '06-06-2021 00:00:00', 
+          content: 'good tweet!'
+        },
+        {
+          id: 6, 
+          publisher:8, 
+          tweet_id: 3, 
+          publish_date: '06-06-2021 00:00:00', 
+          content: 'good tweet!'
+        },
+        {
+          id: 7, 
+          publisher:6, 
+          tweet_id: 4, 
+          publish_date: '06-06-2021 00:00:00', 
+          content: 'good tweet!'
+        },
+        {
+          id: 8, 
+          publisher:1, 
+          tweet_id: 1, 
+          publish_date: '06-06-2021 00:00:00', 
+          content: 'good tweet!'
+        },
       ]);
     });
 };
