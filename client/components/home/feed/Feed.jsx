e8da22df264bcd6cf8b0fdfdbcafd2355a6212ab
matthew-hocking <matthocking2439@gmail.com>
2021-09-24 12:05:53 +1200

    who to follow basic done

--- client/components/home/feed/Feed.jsx
+++ client/components/home/feed/Feed.jsx
@@ -19,7 +19,7 @@ const Feed = () => {
             </div>
           </div>
           <div className="make-tweet-card">
-            <div className="tweet-container">
+            <div className="make-tweet-container">
               <div className="profile-image">
                 <img src="default_image.png" />
               </div>
@@ -85,7 +85,87 @@ const Feed = () => {
           </div>
           <div className="feed-content-conatiner">
             <div className="feed-conent">
-              <h2>whassup</h2>
+              <div className="tweet-container">
+                <div className="tweet">
+                  <div className="profile-image">
+                    <img src="default_image.png" />
+                  </div>
+                  <div className="user-info">
+                    <a href="">
+                    <span className="user-name">BigRig</span>
+                    <span className="user-handle"> @BigRig12345678</span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="wtf-container">
+                <div className="wtf-header">
+                  <div className="wtf-title">
+                    <h1>Who to follow</h1>
+                  </div>
+                  <div className="wtf-more">
+                    <svg viewBox="0 0 24 24" aria-hidden="true" className="more">
+                      <g>
+                        <circle cx="5" cy="12" r="2"></circle>
+                        <circle cx="12" cy="12" r="2"></circle>
+                        <circle cx="19" cy="12" r="2"></circle>
+                      </g>
+                    </svg>
+                  </div>
+                </div>
+                <div className="wtf-user-list-container">
+                  <div className="wtf-user-container">
+                    <div className="wtf-user">
+                      <div className="profile-image">
+                        <img src="default_image.png" />
+                      </div>
+                      <div className="user-info">
+                        <div className="user-deets">
+                          <a href="">
+                          <span className="user-name">BigRig</span>
+                          <span className="user-handle"> @BigRig12345678</span>
+                          </a>
+                        </div>
+                        <div className="user-follow">
+                          <button>Follow</button>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="wtf-user">
+                      <div className="profile-image">
+                        <img src="default_image.png" />
+                      </div>
+                      <div className="user-info">
+                        <div className="user-deets">
+                          <a href="">
+                          <span className="user-name">BigRig</span>
+                          <span className="user-handle"> @BigRig12345678</span>
+                          </a>
+                        </div>
+                        <div className="user-follow">
+                          <button>Follow</button>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="wtf-user">
+                      <div className="profile-image">
+                        <img src="default_image.png" />
+                      </div>
+                      <div className="user-info">
+                        <div className="user-deets">
+                          <a href="">
+                          <span className="user-name">BigRig</span>
+                          <span className="user-handle"> @BigRig12345678</span>
+                          </a>
+                        </div>
+                        <div className="user-follow">
+                          <button>Follow</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
