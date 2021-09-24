7f3d988576e4bfa5e9e6a985aa5c208901004464
Jordan Cooper <jordancooper1195@gmail.com>
2021-09-24 17:48:55 +1200

    brining in reducers into the combiner

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -3,9 +3,13 @@ import { combineReducers } from 'redux'
 import userReducer from './userReducer'
 import tweetsReducer from './tweetsReducer'
 import authReducer from './authReducer'
+import followersReducer from './followersReducer'
+import followingReducer from './followingReducer'
 
 export default combineReducers({
   user: userReducer,
   tweet: tweetsReducer,
-  auth: authReducer
+  auth: authReducer,
+  followers: followersReducer,
+  following: followingReducer
 })
