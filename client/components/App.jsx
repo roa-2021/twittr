05d6e845f9fdbda977b8b0d790bda30d291d897e
Maximilon Baddeley <maximilon@Maximilons-iMac.local>
2021-09-26 19:46:32 +1300

    add login

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,21 +3,14 @@ import React from 'react'
 import Landing from './home/landing/Landing'
 import Home from './home/Home'
 
-import LoginTrueForm from './home/landing/LoginTrueForm'
-import LoginForm from './home/landing/LoginForm'
-
 import Logout from './home/Logout'
 
-
-function App () {
+function App() {
   return (
     <>
-
-      <LoginForm />
-      {/* <Landing /> */}
-      {/* <Home /> */}  
+      <Landing />
+      {/* <Home /> */}
       {/* <Logout /> */}
-
     </>
   )
 }
--- client/components/home/landing/Landing.jsx
+++ client/components/home/landing/Landing.jsx
@@ -2,18 +2,16 @@ import React, { Link } from 'react'
 import ImagePanel from './ImagePanel'
 import LoginPanel from './LoginPanel'
 import FooterPanel from './FooterPanel'
-import LoginForm from './LoginForm'
 
 const Landing = () => {
   return (
     <>
       <div className="landing-login-container">
         <div className="landing-container">
-          <LoginForm />
-          {/* <ImagePanel /> */}
-          {/* <LoginPanel /> */}
+          <ImagePanel />
+          <LoginPanel />
         </div>
-        {/* <FooterPanel /> */}
+        <FooterPanel />
       </div>
     </>
   )
