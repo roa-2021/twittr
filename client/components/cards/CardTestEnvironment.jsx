5db6470b3909c875657ca29f5f3e8665a458080b
gordon-fong <85857853+gordon-fong@users.noreply.github.com>
2021-09-26 21:09:43 +1300

    add logout card to test environment

--- client/components/cards/CardTestEnvironment.jsx
+++ client/components/cards/CardTestEnvironment.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 
 import BadgeCard from './BadgeCard'
 import TweetCard from './TweetCard'
+import LogoutCard from './LogoutCard'
 
 const CardTestEnvironment = () => {
   return (
     <div className="card-test-page">
+      <div className="card-test-page__card-container">
+        <LogoutCard />
+      </div>
       <div className="card-test-page__card-container">
         <BadgeCard />
       </div>
