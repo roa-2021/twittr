fa550418c66337ca461ab7573bed92aa0fae841d
Patrick John Sullivan <17734388+patrick-j-sulley@users.noreply.github.com>
2021-09-27 21:29:24 +1300

    WIP Pop up delete tweet card

--- client/components/cards/ConfirmationDeleteTweetCard.jsx
+++ client/components/cards/ConfirmationDeleteTweetCard.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 
 import { connect } from 'react-redux'
 
-import {removeATweetByIDTHUNK} from '../../actions/tweetsActions'
+import { removeATweetByIDTHUNK } from '../../actions/tweetsActions'
 
 import { resetAppTHUNK } from '../../actions/RESET_APP'
 
-const ConfirmationDeleteTweetCard = ({dispatch, i, toggleDel}) => {
+const ConfirmationDeleteTweetCard = ({ dispatch, i, toggleDel }) => {
 
   const deleteTweet = i => {
     console.log(i)
@@ -15,26 +15,27 @@ const ConfirmationDeleteTweetCard = ({dispatch, i, toggleDel}) => {
     toggleDel()
   }
 
-  return (
-    <div className="popup-card-container">
-      <div className="popup-card-container__header-text-container">
-        <span>Delete Tweet?</span>
-      </div>
-      <div className="popup-card-container__paragraph-text-container">
-        <span>
-          This can’t be undone and it will be removed from your profile, the timeline of any accounts that follow you, and from Twitter search results. 
-        </span>
-      </div>
-      <Link to="/home">
-        <div className="popup-card-container__red-button" onClick={() => deleteTweet(i)}>
-          <span>Delete</span>
+  const ConfirmationDeleteTweetCard = ({ toggleDel }) => {
+    return (
+      <div className="popup-card-container">
+        <div className="popup-card-container__header-text-container">
+          <span>Delete Tweet?</span>
         </div>
-      </Link>
+        <div className="popup-card-container__paragraph-text-container">
+          <span>
+            This can’t be undone and it will be removed from your profile, the timeline of any accounts that follow you, and from Twitter search results.
+          </span>
+        </div>
+        <Link to="/home">
+          <div className="popup-card-container__red-button" onClick={() => deleteTweet(i)}>
+            <span>Delete</span>
+          </div>
+        </Link>
         <div className="popup-card-container__light-button" onClick={toggleDel}>
           <span>Cancel</span>
         </div>
-    </div>
-  )
+      </div>
+    )
+  }
 }
-
 export default connect()(ConfirmationDeleteTweetCard)
