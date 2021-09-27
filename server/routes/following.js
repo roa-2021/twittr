5d29ad308b51f3dd2a957c18992f7e14f83d42f9
maximilon <maximilonbaddeley@gmail.com>
2021-09-28 11:46:03 +1300

    display list of followers and following from redux

--- server/routes/following.js
+++ server/routes/following.js
@@ -3,7 +3,7 @@ const db = require('../db/db')
 const router = express.Router()
 
 router.get('/:id',(req,res)=>{
-  userID=req.params.id
+  const userID = req.params.id
   db.getFollowing(userID)
   .then(following=>{
     res.json(following)
