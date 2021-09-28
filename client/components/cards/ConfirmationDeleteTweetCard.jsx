90937fdb57e0d41ed7a9b116db627d4c8f791bc8
Patrick John Sullivan <17734388+patrick-j-sulley@users.noreply.github.com>
2021-09-28 13:11:00 +1300

    Resolved local merge conflicts after rebasing with dev

--- client/components/cards/ConfirmationDeleteTweetCard.jsx
+++ client/components/cards/ConfirmationDeleteTweetCard.jsx
@@ -15,27 +15,31 @@ const ConfirmationDeleteTweetCard = ({ dispatch, i, toggleDel }) => {
     toggleDel()
   }
 
-  const ConfirmationDeleteTweetCard = ({ toggleDel }) => {
-    return (
-      <div className="popup-card-container">
-        <div className="popup-card-container__header-text-container">
-          <span>Delete Tweet?</span>
-        </div>
-        <div className="popup-card-container__paragraph-text-container">
-          <span>
-            This can’t be undone and it will be removed from your profile, the timeline of any accounts that follow you, and from Twitter search results.
-          </span>
-        </div>
-        <Link to="/home">
-          <div className="popup-card-container__red-button" onClick={() => deleteTweet(i)}>
-            <span>Delete</span>
-          </div>
-        </Link>
-        <div className="popup-card-container__light-button" onClick={toggleDel}>
-          <span>Cancel</span>
-        </div>
+  return (
+    <div className="popup-card-container">
+      <div className="popup-card-container__header-text-container">
+        <span>Delete Tweet?</span>
       </div>
-    )
-  }
+      <div className="popup-card-container__paragraph-text-container">
+        <span>
+          This can’t be undone and it will be removed from your profile, the timeline of any accounts that follow you, and from Twitter search results.
+        </span>
+      </div>
+      <Link to="/home">
+      <div className="popup-card-container__red-button" onClick={() => deleteTweet(i)}>
+      <span>Delete</span>
+      </div>
+      </Link>
+      <Link to="/home">
+      <div className="popup-card-container__red-button" onClick={() => deleteTweet(i)}>
+      <span>Delete</span>
+      </div>
+      </Link>
+      <div className="popup-card-container__light-button" onClick={toggleDel}>
+      <span>Cancel</span>
+      </div>
+    </div>
+  )
 }
+
 export default connect()(ConfirmationDeleteTweetCard)
